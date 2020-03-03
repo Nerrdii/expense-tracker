ffabcc6405ea9aad3dc56258130f6576780a60c7
Joseph Davis <joseph.davis@hrblock.com>
2020-03-03 15:01:40 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDb = require('./config/db');
+require('colors');
+
+dotenv.config({ path: './config/config.env' });
+
+connectDb();
+
+const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use('/api/v1/transactions', require('./routes/transactions'));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(
+    `Server started on port ${PORT} in ${process.env.NODE_ENV} mode`.yellow.bold
+  )
+);
