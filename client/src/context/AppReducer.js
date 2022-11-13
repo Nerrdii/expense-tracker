b227f6a5b7edaf0794d90323cf207537305a3710
Joseph Davis <joseph.n.davis.98@gmail.com>
2022-11-13 14:35:14 -0600

    Update deps

--- client/src/context/AppReducer.js
+++ client/src/context/AppReducer.js
@@ -1,29 +1,31 @@
-export default (state, action) => {
+const reducer = (state, action) => {
   switch (action.type) {
     case 'GET_TRANSACTIONS':
       return {
         ...state,
         loading: false,
-        transactions: action.payload
+        transactions: action.payload,
       };
     case 'ADD_TRANSACTION':
       return {
         ...state,
-        transactions: [...state.transactions, action.payload]
+        transactions: [...state.transactions, action.payload],
       };
     case 'DELETE_TRANSACTION':
       return {
         ...state,
         transactions: state.transactions.filter(
-          transaction => transaction._id !== action.payload
-        )
+          (transaction) => transaction._id !== action.payload
+        ),
       };
     case 'TRANSACTION_ERROR':
       return {
         ...state,
-        error: action.payload
+        error: action.payload,
       };
     default:
       return state;
   }
 };
+
+export default reducer;
