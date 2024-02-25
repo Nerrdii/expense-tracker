b2de72934f4bca4fa1f42711c8be0447a729d099
Joseph Davis <joseph.n.davis.98@gmail.com>
2024-02-25 15:34:00 -0600

    Remove unnecessary id

--- client/src/components/AddTransaction.js
+++ client/src/components/AddTransaction.js
@@ -6,13 +6,12 @@ export const AddTransaction = () => {
   const [amount, setAmount] = useState(0);
   const { addTransaction } = useContext(GlobalContext);
 
-  const onSubmit = e => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
     const newTransaction = {
-      id: Math.floor(Math.random() * 100000000),
       text,
-      amount: +amount
+      amount: +amount,
     };
 
     addTransaction(newTransaction);
@@ -27,7 +26,7 @@ export const AddTransaction = () => {
           <input
             type="text"
             value={text}
-            onChange={e => setText(e.target.value)}
+            onChange={(e) => setText(e.target.value)}
             placeholder="Enter text..."
           />
         </div>
@@ -39,7 +38,7 @@ export const AddTransaction = () => {
           <input
             type="number"
             value={amount}
-            onChange={e => setAmount(e.target.value)}
+            onChange={(e) => setAmount(e.target.value)}
             placeholder="Enter amount..."
           />
         </div>
